45f3603138f13fef961111f136c982694d49dad8
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-07-04 15:51:47 +0900

    2020/07/04
    리스트

--- my_first_python/Unit2/practice_3.py
+++ my_first_python/Unit2/practice_3.py
@@ -1,70 +1,75 @@
-###### 변수
-message = "Hello"
-message = "world" #변수는 바꿀 수 있다
-print(message)
+##### 변수 #####
 
-# 변수 이름 짓기
-# 숫자로 시작하면 안된다.
-# 띄어쓰기는 _로 한다.
-# 파이썬 키워드나 함수는 안된다.
-# 이름의 뜻하는 바가 명확해야 한다.
+#  변수 선언
+name = 'Lee'
+name = 'seung jae' # 변수의 값은 변할 수 있다
 
-##### 문자
-# 선언법
-# "string"
-# 'string'
-# "I'm string"
+# 변수 이름
+# 1. 숫자로 시작하면 안된다.
+# 2. 띄어쓰기는 _로 구분
+# 3. 키워드, 함수명은 안된다.
+# 4. 갖고 있는 값을 명확히 설명하는 이름으로
+
+
+##### 문자열 #####
+
+# 문자 만드는 법
+'Hello' 
+"Hello"
+"I'm seung jae"
+
+# 문자 연결
+first_name = 'lee'
+last_name = 'seung jae'
+full_name = first_name + ' ' + last_name
 
 # 대소문자 변경
 name = 'Lee seungjae'
-print(name.title()) # 앞글자만 대문자
+print(name.title()) # 단어별 앞글자만 대문자
 print(name.upper()) # 모든 글자 대문자
 print(name.lower()) # 모든 글자 소문자
 
 # 공백 문자
-print("\tPython") # 들여쓰기
-print("\nPython") # 줄바꿈
-
-# 문자열 연결
-first_name = 'Lee'
-last_name = 'seungjae'
-full_naem = first_name + " " + last_name
+print('\npython') # 줄바꿈
+print('\tpython') # 들여쓰기
 
 # 공백 잘라내기
-language = ' Python '
-language.lstrip() # 왼쪽 공백 제거
-language.rstrip() # 오른쪽 공백 제거
-language.strip() # 양쪽 모든 공백 제거
+language = ' python '
+print(language.strip()) # 앞 뒤 공백 제거
+print(language.lstrip()) # 왼쪽 공백 제거
+print(language.rstript()) # 오른쪽 공백 제거
 
 
-##### 숫자
-
-# 정수형과 부동소수점 숫자가 있다
+##### 숫자 #####
 
 # 정수 연산
-print(1 + 2)
-print(1 - 2)
-print(1 * 2)
-print(1 / 2)
-print(1 ** 2)
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+print(2 ** 3)
 
-# 부동소수점 연산
-print(0.2 + 0.2)
+# 연산 우선순위
+print((2 + 3) * 4)
+
+# 부동소수점 숫자 연산
+print(0.3 + 0.1)
 
 # 이상한 수
 print(0.2 + 0.1)
 
-# 문자열 결합시 에러 피하기 (숫자를 문자로 변환하기)
+# 문자와 연결시 오류 (숫자를 문자로 변환)
 age = 28
-message = "I'm " + str(age) +" years old."
+message = "Hello, i' " + age + ' years old.'
 print(message)
 
 
-##### 주석
-# 주석은 간결하고 명료해야 한다.
+##### 주석 #####
+# 주석은 간결하고 명료하게
 
-##### 파이썬 철학
-# 추함보다 아름다움이 낫다
-# 복잡함보다 단순함이 낫다
-# 가독성이 중요하다
-# 할 일은 지금 시작하라
+
+##### 파이썬 철학 #####
+# 1. 추함보다 아름다움이 낫습니다.
+# 2. 복잡함보다 단순함이 낫습니다.
+# 3. 가독성에 신경쓰세요.
+# 4. 할 일은 지금 당장 하세요.
--- my_first_python/Unit3/1_list_basic.py
+++ my_first_python/Unit3/1_list_basic.py
@@ -1,3 +1,23 @@
-##### 리스트 사용법 #####
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles)
+# 리스트 선언
+# friends = ['승재', '명준', '규성']
+
+# # 리스트 요소 접근 및 수정
+# friends[0] = '재윤'
+# friends[-1] = '재윤' # 마지막 요소
+ 
+# # 리스트 추가
+# friends.append('상진') # 마지막에 추가
+# friends.insert(0, '상진') # 0번 인덱스에 추가
+
+# # 리스트 제거
+# del friends[0] # 0번 인덱스 제거
+# popped_friend = friends.pop(3) # 3번 인덱스 제거후 변수저장
+# friends.remove('재윤') # 값이 재윤인 인덱스 제거
+
+# print(friends)
+# print(popped_friend)
+
+# 리스트 정렬
+fruits = ['banana', 'apple', 'watermelon', 'pineapple', 'mango']
+
+print(len(fruits))
